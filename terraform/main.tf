2a562179fc889a44bf81dc578524176d1d5e8c57
Martin Bružina <martin@bruzina.cz>
2026-03-22 22:53:30 +0100

    feat: add repository metadata

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,4 +6,9 @@ resource "github_repository" "this" {
   for_each = { for repository in local.config.repositories : repository.name => repository }
 
   name = each.value.name
+
+  # Metadata
+  description  = try(each.value.description, null)
+  homepage_url = try(each.value.homepage_url, null)
+  topics       = try(each.value.topics, null)
 }
